c9a9dec32c860cfb1c58078a8b34907a088531f8
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-15 16:53:29 +0700

    up

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 const db = require("./models");
 
-//Routers
+// Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
+
 const commentsRouter = require("./routes/Comments");
 app.use("/comments", commentsRouter);
+
 const userRouter = require("./routes/Users");
 app.use("/auth", userRouter);
+
 const likesRouter = require("./routes/Likes");
 app.use("/likes", likesRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("server running");
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 });
